950b3de868a55330ae1142e00608279981f95764
“kudos” <hiroyuki.kudou@n-resolution.jp>
2023-01-18 15:32:25 +0900

    test

--- src/main/java/com/example/demo/users/UsersController.java
+++ src/main/java/com/example/demo/users/UsersController.java
@@ -1,14 +1,26 @@
 package com.example.demo.users;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/users")
 public class UsersController {
-  @RequestMapping("/users")
-  public String demo(Model model) {
-    model.addAttribute("message", "index");
+  @Autowired
+  private JdbcTemplate jdbcTemplate;
+	
+  @GetMapping
+  public String index(Model model) {
+    String sql = "SELECT * FROM users";
+    List<Map<String, Object>> users = jdbcTemplate.queryForList(sql);
+    model.addAttribute("users", users);
     return "users/index";
   }
 }
