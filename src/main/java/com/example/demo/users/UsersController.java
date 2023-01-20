e7c993812d9624f2fb1d8dcc2b0d5717c6b82b73
“kudos” <hiroyuki.kudou@n-resolution.jp>
2023-01-20 09:54:50 +0900

    test

--- src/main/java/com/example/demo/users/UsersController.java
+++ src/main/java/com/example/demo/users/UsersController.java
@@ -2,13 +2,16 @@ package com.example.demo.users;
 
 import java.util.List;
 import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import com.example.demo.users.dto.request.UsersCreateRequestDto;
+
 
 @Controller
 @RequestMapping("/users")
@@ -22,5 +25,10 @@ public class UsersController {
     List<Map<String, Object>> users = jdbcTemplate.queryForList(sql);
     model.addAttribute("users", users);
     return "users/index";
+  }
+  
+  @GetMapping("/create")
+  public String create(@ModelAttribute UsersCreateRequestDto usersCreateRequestDto) {
+    return "users/create";
   }
 }
---
+++ src/main/java/com/example/demo/users/dto/request/UsersCreateRequestDto.java
@@ -0,0 +1,8 @@
+package com.example.demo.users.dto.request;
+
+import lombok.Data;
+
+@Data
+public class UsersCreateRequestDto {
+  private String id;
+}
