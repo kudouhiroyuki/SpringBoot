920e7946ccabfabe1f577bf3490c4ef14b15724e
“kudou” <h.kudou@ebacorp.jp>
2023-04-17 07:27:45 +0900

    test

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 http://localhost:8080/user<br>
 
+「Web」→「Spring Web」
+「Developer Tools」→「Spring Boot DevTools」
+「Template Engine」→「Thymeleaf」
+
 ```
 System.out.println("test");
 ```
--- src/main/java/com/example/demo/TopController.java
+++ src/main/java/com/example/demo/TopController.java
@@ -5,7 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class TopController {
+public class TopController {	
   @GetMapping("/")
   public String demo(Model model) {
     model.addAttribute("message", "index");
