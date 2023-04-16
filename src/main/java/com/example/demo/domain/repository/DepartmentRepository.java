9cc476d0f15a5e2045f78a00f03a00da842bc180
“kudou” <h.kudou@ebacorp.jp>
2023-04-17 07:04:49 +0900

    test

--- src/main/java/com/example/demo/domain/repository/DepartmentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.domain.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.domain.entity.DepartmentEntity;
-
-public interface DepartmentRepository extends JpaRepository<DepartmentEntity,Integer>{
-}
--- src/main/java/com/example/demo/user/dto/request/UserCreateRequestDto.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.user.dto.request;
-
-import lombok.Data;
-
-@Data
-public class UserCreateRequestDto {
-  private String id;
-}
