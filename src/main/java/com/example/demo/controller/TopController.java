a44e5d0c1529b436fb48cc5974efe6d391c51ffb
“kudou” <h.kudou@ebacorp.jp>
2023-04-22 11:29:58 +0900

    test

--- src/main/java/com/example/demo/controller/TopController.java
+++ src/main/java/com/example/demo/controller/TopController.java
@@ -1,16 +1,17 @@
-package com.example.demo.user;
+package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-@RequestMapping("/user") 
-public class UserController {
+@RequestMapping("/") 
+public class TopController {	
+  
   @GetMapping
   public String index(Model model) {
-    model.addAttribute("message", "UserController index");   
+    model.addAttribute("message", "TopController index");   
     return "index";
   }
 }
