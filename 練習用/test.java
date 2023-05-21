76e7015d088c1fba39d6f1d6650352b0946844f7
“kudou” <h.kudou@ebacorp.jp>
2023-05-22 07:22:51 +0900

    test

--- 練習用/test.java
+++ 練習用/test.java
@@ -1,14 +1,21 @@
 class Test {
   public static void main(String[] args) {
     System.out.println("Test");
-
-    int sum = 58 + 72 + 90;
-    System.out.println("合計：" + sum);
     
-    int old, height;
+    String name;
+    int old;
+    int height;
+    name = "Tarou Yamada";
     old = 20;
     height = 184;
+    System.out.println("名前：" + name);
     System.out.println("年齢：" + old);
     System.out.println("身長：" + height);
+
+    int sum = 0;
+    for (int i = 1; i <= 5; i++) {
+      sum += i;
+    }
+    System.out.println("合計：" + sum);
   }
 }
