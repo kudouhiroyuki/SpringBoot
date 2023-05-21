5759fa52291567883fe4a3217b9c3c855387be19
“kudou” <h.kudou@ebacorp.jp>
2023-05-22 07:11:16 +0900

    test

--- 練習用/test.java
+++ 練習用/test.java
@@ -1,8 +1,14 @@
 class Test {
   public static void main(String[] args) {
-    System.out.println("Hello");
+    System.out.println("Test");
 
     int sum = 58 + 72 + 90;
-    System.out.println("合計:" + sum);
+    System.out.println("合計：" + sum);
+    
+    int old, height;
+    old = 20;
+    height = 184;
+    System.out.println("年齢：" + old);
+    System.out.println("身長：" + height);
   }
 }
