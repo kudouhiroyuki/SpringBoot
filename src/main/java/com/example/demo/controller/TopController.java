383468e3743db7c0a430c9ebbba94c4c77f859fd
“kudou” <h.kudou@ebacorp.jp>
2023-05-27 08:42:52 +0900

    test

--- src/main/java/com/example/demo/controller/TopController.java
+++ src/main/java/com/example/demo/controller/TopController.java
@@ -2,23 +2,16 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import java.util.Map;
 
-@RestController
+@Controller
 @RequestMapping("/") 
 public class TopController {
 
   @GetMapping
-  public Map<String, String> index() {
-    return Map.of("key", "value");
+  public String index(Model model) {
+    model.addAttribute("message", "TopController index");   
+    return "index";
   }
-	
-//  @GetMapping
-//  public String index(Model model) {
-//    model.addAttribute("message", "TopController index");   
-//    return "index";
-//  }
 }
