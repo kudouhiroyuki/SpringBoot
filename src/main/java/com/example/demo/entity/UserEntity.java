91527f8797714f3c881ca6b1b9a8c04f046cf378
“kudou” <h.kudou@ebacorp.jp>
2023-07-04 08:28:15 +0900

    test

--- src/main/java/com/example/demo/entity/UserEntity.java
+++ src/main/java/com/example/demo/entity/UserEntity.java
@@ -11,6 +11,8 @@ import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Pattern;
 
 import java.sql.Date;
 import lombok.Data;
@@ -24,27 +26,34 @@ public class UserEntity {
   int id;
   
   @NotEmpty(message = "名前を入力してください")
-  @Column(name = "user_name", nullable = false, length = 60)
   @Size(min = 1, max = 60, message = "名前は60文字以内で入力してください")
+  @Pattern(regexp = "^[\\u4E00-\\u9FFF]+$", message = "名前は漢字で入力してください")
+  @Column(name = "user_name", nullable = false, length = 60)
   private String userName;
   
   @NotEmpty(message = "パスワードを入力してください")
-  @Column(name = "password", nullable = false, length = 255)
+  @Size(min = 8, max = 100, message = "パスワードは8〜100文字以内で入力してください")
+  @Pattern(regexp = "^[!-~]+$", message = "パスワードは数字・英字・記号で入力してください")
+  @Column(name = "password", nullable = false, length = 100)
   private String password;
   
   @NotEmpty(message = "メールアドレスを入力してください")
   @Email(message = "メールアドレスを正しく入力してください")
-  @Column(name = "address", nullable = false, length = 191)
+  @Size(min = 1, max = 255, message = "メールアドレスは255文字以内で入力してください")
+  @Column(name = "address", nullable = false, length = 255)
   private String address;
   
-  @Column(name = "age", nullable = false, length = 191)
+  @NotEmpty(message = "年齢を入力してください")
+  @Positive(message = "年齢を正しく入力してください")
+  @Column(name = "age", nullable = false)
   private String age;
  
   @ManyToOne
   @JoinColumn(name = "department_id")
   private DepartmentEntity department;
   
-  @Column(name = "department_id", nullable = false, length = 191, updatable = false, insertable = false)
+  @NotEmpty(message = "部署を入力してください")
+  @Column(name = "department_id", nullable = false, length = 30, updatable = false, insertable = false)
   private String departmentId;
   
   @Column(name = "point", nullable = false)
