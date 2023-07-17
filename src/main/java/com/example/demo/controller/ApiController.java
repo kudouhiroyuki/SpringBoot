8c90411b840d5fecb76370f8a668f09f2ddcf8d8
“kudou” <h.kudou@ebacorp.jp>
2023-07-18 07:40:29 +0900

    test

--- src/main/java/com/example/demo/controller/ApiController.java
+++ src/main/java/com/example/demo/controller/ApiController.java
@@ -1,18 +1,39 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.CommentEntity;
+import com.example.demo.entity.PostEntity;
+
+import java.sql.Date;
+
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.Data;
+
+@Data
+class Test2Entity {
+  private String message;
+}
 
 @RestController
 @RequestMapping("/api") 
 public class ApiController {
 
-  @GetMapping
-  public String getApiTest(Model model) {
-    return "test";
+  @GetMapping("/test1")
+  public String getTest1(Model model) {
+    return "test1";
+  }
+  
+  @GetMapping("test2")
+  public Test2Entity getTest2(Model model) {
+    Test2Entity result = new Test2Entity();
+    result.setMessage("test2");
+    return result;
   }
 }
 
-// curl -v -X GET "http://localhost:8080/api"
+// curl -v -X GET "http://localhost:8080/api/test1"
+// curl -v -X GET "http://localhost:8080/api/test2"
--- src/main/java/com/example/demo/entity/CommentEntity.java
+++ src/main/java/com/example/demo/entity/CommentEntity.java
@@ -9,13 +9,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Column;
 import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Size;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Pattern;
-
-import jakarta.persistence.CascadeType;
-
 
 import java.sql.Date;
 import lombok.Data;
