64418a7e267f682952ba1848630e3f996c2e4cac
“kudou” <h.kudou@ebacorp.jp>
2023-07-18 08:15:19 +0900

    test

--- src/main/java/com/example/demo/controller/ApiController.java
+++ src/main/java/com/example/demo/controller/ApiController.java
@@ -4,9 +4,14 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import lombok.Data;
 
+@Data
+class TestRequest {
+  private String message;
+}
 @Data
 class TestEntity {
   private String message;
@@ -26,21 +31,33 @@ public class ApiController {
     return message;
   }
   
-  @GetMapping("test3")
-  public TestEntity getTest3() {
+  @GetMapping("/test3")
+  public TestRequest getTest3(TestRequest request) {
+    return request;
+  }
+
+  @GetMapping("/test4/{id}")
+  public String getTest4(@PathVariable String id) {
+    return id;
+  }
+  
+  @GetMapping("test5")
+  public TestEntity getTest5() {
     TestEntity result = new TestEntity();
     return result;
   }
   
-  @GetMapping("test4")
-  public TestEntity getTest4() {
+  @GetMapping("test6")
+  public TestEntity getTest6() {
     TestEntity result = new TestEntity();
-    result.setMessage("test4");
+    result.setMessage("test6");
     return result;
   }
 }
 
 // curl -v -X GET "http://localhost:8080/api/test1"
 // curl -v -X GET "http://localhost:8080/api/test2?message=test2"
-// curl -v -X GET "http://localhost:8080/api/test3"
-// curl -v -X GET "http://localhost:8080/api/test4"
+// curl -v -X GET "http://localhost:8080/api/test3?message=test3"
+// curl -v -X GET "http://localhost:8080/api/test4/1"
+// curl -v -X GET "http://localhost:8080/api/test5"
+// curl -v -X GET "http://localhost:8080/api/test6"
