2813ead7ff49c992924d31eb444148482f961705
“kudou” <h.kudou@ebacorp.jp>
2023-07-20 07:28:06 +0900

    test

--- src/main/java/com/example/demo/controller/ApiController.java
+++ src/main/java/com/example/demo/controller/ApiController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
 import lombok.Data;
 
 import com.example.demo.service.EmployeeService;
@@ -65,13 +67,38 @@ public class ApiController {
     return result;
   }
   
+  @GetMapping("test7")
+  public ResponseEntity<String> getTest7() {
+    return new ResponseEntity<>("test7", HttpStatus.OK);
+  }
+  
+  @GetMapping("test8")
+  public ResponseEntity<String> getTest8() {
+    return ResponseEntity.ok().body("test8");
+  }
+  
+  @GetMapping("test9")
+  public ResponseEntity<String> getTest9() {
+    return ResponseEntity.status(HttpStatus.OK).body("test9");
+  }
+  
   @GetMapping("error1")
-  public void getError1() {
-    throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+  public ResponseEntity<String> getError1() {
+    return ResponseEntity.badRequest().body("Bad Request");
   }
   
   @GetMapping("error2")
-  public void getError2() {
+  public ResponseEntity<String> getError2() {
+    return ResponseEntity.notFound().build();
+  }
+  
+  @GetMapping("error3")
+  public void getError3() {
+    throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+  }
+  
+  @GetMapping("error4")
+  public void getError4() {
     throw new ResponseStatusException(HttpStatus.NOT_FOUND);
   }
   
@@ -88,9 +115,14 @@ public class ApiController {
 // curl -v -X GET "http://localhost:8080/api/test4/1"
 // curl -v -X GET "http://localhost:8080/api/test5"
 // curl -v -X GET "http://localhost:8080/api/test6"
+// curl -v -X GET "http://localhost:8080/api/test7"
+// curl -v -X GET "http://localhost:8080/api/test8"
+// curl -v -X GET "http://localhost:8080/api/test9"
 
 // curl -v -X GET "http://localhost:8080/api/error1"
 // curl -v -X GET "http://localhost:8080/api/error2"
+// curl -v -X GET "http://localhost:8080/api/error3"
+// curl -v -X GET "http://localhost:8080/api/error4"
 
 // curl -v -X GET "http://localhost:8080/api/employees"
 
