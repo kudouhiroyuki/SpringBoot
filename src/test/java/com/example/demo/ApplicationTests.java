d6a363f2cc7a9c75567f59bf30780d3abedc04e5
“kudou” <h.kudou@ebacorp.jp>
2023-07-22 15:57:34 +0900

    test

--- src/test/java/com/example/demo/ApplicationTests.java
+++ src/test/java/com/example/demo/ApplicationTests.java
@@ -2,12 +2,25 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.Assert.*;
+
+
+class Sample1 {
+  public String result() {
+    return "test";
+  }
+}
 
 @SpringBootTest
 class ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {
+  }
+  
+  @Test
+  public void Sample1Test() {
+    Sample1 sample1 = new Sample1();
+    assertEquals(sample1.result(), "test");
+  }
 }
