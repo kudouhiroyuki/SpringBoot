de90161a60f14918272217a71c800f00a011db37
“kudou” <h.kudou@ebacorp.jp>
2023-07-25 08:28:50 +0900

    test

---
+++ src/test/java/com/example/demo/ApiControllerTest.java
@@ -0,0 +1,41 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ApiControllerTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+  
+  @Test
+  void getTest1() throws Exception {
+    this.mockMvc.perform(get("/api/test1"))
+      .andExpect(status().isOk())
+      .andExpect(content().string("test1"));
+  }
+  
+  @Test
+  void getTest2() throws Exception {
+    this.mockMvc.perform(get("/api/test2").param("param1", "test2"))
+      .andExpect(status().isOk())
+      .andExpect(content().string("test2"));
+  }
+  
+  @Test
+  void getTest3() throws Exception {
+    this.mockMvc.perform(get("/api/test3").param("param1", "test3"))
+      .andExpect(status().isOk())
+      .andExpect(content().json("{param1: test3}"));
+  }
+}
