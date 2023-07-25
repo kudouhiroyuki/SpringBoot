196efb86fc87f17abeaf435adb7fadfa970754c9
“kudou” <h.kudou@ebacorp.jp>
2023-07-26 08:52:09 +0900

    test

--- src/test/java/com/example/demo/ApiControllerTest.java
+++ src/test/java/com/example/demo/ApiControllerTest.java
@@ -6,10 +6,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.CoreMatchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.example.demo.entity.EmployeeEntity;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -37,5 +41,20 @@ class ApiControllerTest {
     this.mockMvc.perform(get("/api/test3").param("param1", "test3"))
       .andExpect(status().isOk())
       .andExpect(content().json("{param1: test3}"));
+  }
+  
+  @Test
+  void getTest4() throws Exception {
+    this.mockMvc.perform(get("/api/test4/1"))
+      .andExpect(status().isOk())
+      .andExpect(content().string("1"));
+  }
+  
+  @Test
+  void getEmployeeAll() throws Exception {
+    this.mockMvc.perform(get("/api/employees"))
+      .andExpect(status().isOk());
+//      .andExpect(jsonPath("$.id").value(1));
+//      .andExpect(jsonPath("$.id", is(1)));
   }
 }
