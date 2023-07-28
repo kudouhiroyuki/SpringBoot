d39d8e262947d2bab6a23ab9b1f9155892e0de45
“kudou” <h.kudou@ebacorp.jp>
2023-07-28 17:24:26 +0900

    test

--- src/test/java/com/example/demo/ApiControllerTest.java
+++ src/test/java/com/example/demo/ApiControllerTest.java
@@ -3,7 +3,6 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -53,8 +52,8 @@ class ApiControllerTest {
   @Test
   void getEmployeeAll() throws Exception {
     this.mockMvc.perform(get("/api/employees"))
-      .andExpect(status().isOk());
-//      .andExpect(jsonPath("$.id").value(1));
-//      .andExpect(jsonPath("$.id", is(1)));
+      .andExpect(status().isOk())
+      .andExpect(jsonPath("$[0].id").value(1))
+      .andExpect(jsonPath("$[0].userName").value("名前"));
   }
 }
