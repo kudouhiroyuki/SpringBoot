a6140c4f30562ee890b1297181d36f7f59a29891
“kudou” <h.kudou@ebacorp.jp>
2023-08-01 07:27:03 +0900

    test

--- src/test/java/com/example/demo/ApplicationTest.java
+++ src/test/java/com/example/demo/ApplicationTest.java
@@ -1,16 +1,20 @@
 package com.example.demo;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.Assert.*;
 
 
-class Sample1 {
+class Test2 {
   public String result() {
-    return "test";
+    return "test2";
   }
 }
-class Sample2 {
+class Test3 {
   public String result(String text) {
     return text;
   }
@@ -19,19 +23,37 @@ class Sample2 {
 @SpringBootTest
 class ApplicationTests {
 
-  @Test
-  void contextLoads() {
+  @BeforeAll
+  static void beforeAll() {
+    System.out.println("1番最初に１度だけ実行される。メソッドはstaticである必要がある");
+  }
+  @BeforeEach
+  public void beforeEach() {
+    System.out.println("付与されたメソッドは、各テストメソッドの前に毎回実行される");
+  }
+  @AfterAll
+  static void afterAll() {
+    System.out.println("付与されたメソッドは、1番最後に１度だけ実行される。メソッドはstaticである必要がある");
+  }
+  @AfterEach
+  public void afterEach() {
+    System.out.println("付与されたメソッドは、各テストメソッドの後に毎回実行される");
   }
   
   @Test
-  public void Sample1Test() {
-    Sample1 sample1 = new Sample1();
-    assertEquals(sample1.result(), "test");
+  public void Test1() {
+    assertEquals(12, 12);
   }
   
   @Test
-  public void Sample2Test() {
-    Sample2 sample2 = new Sample2();
-    assertEquals(sample2.result("test"), "test");
+  public void Test2() {
+    Test2 test2 = new Test2();
+    assertEquals(test2.result(), "test2");
+  }
+  
+  @Test
+  public void Test3() {
+    Test3 test3 = new Test3();
+    assertEquals(test3.result("test3"), "test3");
   }
 }
