b967d971e17a42ffb838918dca9d245a4d36666a
“kudou” <h.kudou@ebacorp.jp>
2023-08-17 08:28:10 +0900

    test

--- src/test/java/com/example/demo/ApiControllerTest.java
+++ src/test/java/com/example/demo/ApiControllerTest.java
@@ -6,13 +6,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.CoreMatchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import com.example.demo.entity.EmployeeEntity;
 
 @SpringBootTest
 @AutoConfigureMockMvc
