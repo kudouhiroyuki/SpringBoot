e4be5247331ba6b1ed733c7f514b26bb3d1d944a
“kudou” <h.kudou@ebacorp.jp>
2023-08-25 07:20:23 +0900

    test

--- src/main/java/com/example/demo/repository/NotificationRepository.java
+++ src/main/java/com/example/demo/repository/NotificationRepository.java
@@ -2,8 +2,8 @@ package com.example.demo.repository;
 
 import java.util.List;
 
-import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.entity.NotificationEntity;
@@ -11,7 +11,6 @@ import com.example.demo.entity.NotificationEntity;
 @Repository
 public interface NotificationRepository extends JpaRepository<NotificationEntity,Integer>{
 
-  // 動的クエリーが上手く動かない
-  @Query(value = "SELECT * FROM spring.notification")
+  @Query(value = "SELECT * FROM notification", nativeQuery = true)
   List<NotificationEntity> findNotificationAll();
 }
