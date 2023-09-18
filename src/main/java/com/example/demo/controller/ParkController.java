31a68364838f45ebdfa4e42ad9b92e7ec907f9d2
“kudou” <h.kudou@ebacorp.jp>
2023-09-19 07:54:54 +0900

    test

--- src/main/java/com/example/demo/controller/ParkController.java
+++ src/main/java/com/example/demo/controller/ParkController.java
@@ -19,8 +19,15 @@ public class ParkController {
   @Autowired
   private ParkRepository parkRepository;
 
+  /**
+   * パーク一覧取得API(GET:/v1/parks?id=XXX)
+   * 
+   * パークの一覧を取得します。
+   * 対象のIdの情報を取得します。
+   * Idは複数設定することができ、取得できた情報を全て返却します。
+   */
   @GetMapping
-  public List<ParksEntity> getParks(@RequestParam("ids") List<Integer> ids) {
+  public List<ParksEntity> getParks(@RequestParam("id") List<Integer> ids) {
     // DBからパークの一覧を取得する。
     List<ParksEntity> parks = new ArrayList<>();
     if (ids.size() == 0) {
@@ -37,8 +44,8 @@ public class ParkController {
     }
     return parksResult;
   }
-  // curl -v -X GET "http://localhost:8080/parks?ids="
-  // curl -v -X GET "http://localhost:8080/parks?ids=1,2"
+  // curl -v -X GET "http://localhost:8080/parks?id="
+  // curl -v -X GET "http://localhost:8080/parks?id=1,2"
   
   @GetMapping("/areas")
   public List<ParksEntity> getParksAreas(@RequestParam("park_id") List<Integer> parkIds) {
