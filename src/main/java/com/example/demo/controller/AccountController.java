137ee59a6a987cc28879d66c5d824d7012176cf9
“kudou” <h.kudou@ebacorp.jp>
2023-10-06 15:07:28 +0900

    test

--- src/main/java/com/example/demo/controller/AccountController.java
+++ src/main/java/com/example/demo/controller/AccountController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
 import java.util.Optional;
+import java.util.List;
+import java.util.ArrayList;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +16,24 @@ import com.example.demo.entity.AccountsLargeClassificationsEntity;
 public class AccountController {
   @Autowired
   private AccountLargeClassificationRepository largeAccountRepository;
+
+  /**
+   * 勘定科目大分類一覧取得API(GET:/accounts_large_classifications)
+   * curl -v -X GET "http://localhost:8080/accounts_large_classifications"
+   */
+  @RequestMapping("accounts_large_classifications")
+  public List<AccountsLargeClassificationsEntity> getAccountsLargeClassifications() {
+    // DBから勘定科目大分類の一覧を取得する。
+    List<AccountsLargeClassificationsEntity> largeAccounts = largeAccountRepository.findAll();
+    // 削除されていないものを抽出する。
+    List<AccountsLargeClassificationsEntity> largeAccountsResult = new ArrayList<>();
+    for (AccountsLargeClassificationsEntity largeAccount : largeAccounts) {
+      if (!largeAccount.isDeleted()) {
+        largeAccountsResult.add(largeAccount);
+      }
+    }
+    return largeAccountsResult;
+  }
 
   /**
    * 勘定科目大分類取得API(GET:/accounts_large_classifications/{id})
